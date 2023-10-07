b017fc140e837c6695ec70068a1b32d2522628ca
gwendal le bris <gwen.coding@gmail.com>
2023-10-07 16:08:11 +0200

    Vue Router: scrollBehavior alwas to top

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,10 @@ const router = createRouter({
       name: 'about',
       component: () => import('../views/AboutView.vue')
     }
-  ]
+  ],
+  scrollBehavior (to, from, savedPosition) {
+    return { top: 0 }
+  }
 })
 
 export default router
